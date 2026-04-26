184ef1022248fe3a4198968d55d33e4127098aad
sweet16palz-hub <sweet16palz@gmail.com>
2026-04-26 08:31:12 +0100

    Update waec-pin.js

--- waec-pin.js
+++ waec-pin.js
@@ -1,5 +1,5 @@
-var password = '505'
+var password = '8112'
 password=prompt('VIP: Enter PIN and click OK to View all Answers for todays Subject','ENTER PIN');
-if (password != '505') {
+if (password != '8112') {
 location.href='https://gbamz.com/waec-answers.html';
 }
