2ecb2bd6d50e94d957f616823933cd0ff28b3a07
sweet16palz-hub <sweet16palz@gmail.com>
2026-04-26 08:22:40 +0100

    Create waec-pin.js

---
+++ waec-pin.js
@@ -0,0 +1,5 @@
+var password = 'ENTER PIN'
+password=prompt('VIP: Enter PIN and click OK to View all Answers for todays Subject','505');
+if (password != 'ENTER PIN') {
+location.href='https://gbamz.com/waec-answers.html';
+}
